c992e05fd2780ca59c317618fd8e56ae97192bb1
zurda <m.weizman@gmail.com>
2021-06-03 17:01:55 +0100

    Use environ to hide keys and postgres credentials

--- mymdb/settings.py
+++ mymdb/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mg_4k$j=_6t%a-p9cm!=h!t!cihm-q=n0-#=7_lszy-*001pm^'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'mymdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env('DEFAULT_DB_ENGINE'),
+        'NAME': env('DEFAULT_DB_NAME'),
+        'USER': env('DEFAULT_DB_USER'),
+        'PASSWORD': env('DEFAULT_DB_PASSWORD'),
+        'HOST': env('DEFAULT_DB_HOST'),
+        'PORT': env('DEFAULT_DB_PORT'),
     }
 }
 
