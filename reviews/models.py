c046ed78dcce2c9e7f62203e9abc0dbc27fc225a
zurda <m.weizman@gmail.com>
2021-06-09 16:47:29 +0100

    Create title model and initial migrations

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    abstract = True
+
+
+class Title(TimeStampMixin):
+  name = models.CharField(max_length=1000)
+  release_year = models.IntegerField()
