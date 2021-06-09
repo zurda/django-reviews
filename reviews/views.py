02f4bf9c85310a07548b03313e4a2ed5fd330583
zurda <m.weizman@gmail.com>
2021-06-09 21:01:13 +0100

    Iterate through entries in index.html and show more lists

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,8 @@ from .models import Title, Review
 
 def index(request):
     latest_title_list = Title.objects.order_by('-created_at')[:5]
-    context = {'latest_title_list': latest_title_list}
+    latest_review_list = Review.objects.order_by('-publish_date')[:5]
+    context = {'latest_title_list': latest_title_list, 'latest_review_list': latest_review_list}
     return render(request, 'reviews/index.html', context)
 
 def detail(request, review_id):
