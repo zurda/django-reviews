15c677caab70de0e98d25f5bcb572719cc2e5ef6
zurda <m.weizman@gmail.com>
2021-06-09 19:39:22 +0100

    Add review model

--- reviews/models.py
+++ reviews/models.py
@@ -20,3 +20,17 @@ class Title(TimeStampMixin):
 
   def was_added_recently(self):
     return self.created_at >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Review(TimeStampMixin):
+  title = models.ForeignKey(Title, on_delete=models.CASCADE)
+  headline = models.CharField(max_length=400) 
+  full_text = models.CharField(max_length=10000)
+  publish_date = models.DateTimeField('date published')
+  votes = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.headline
+
+  def was_published_recently(self):
+    return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
